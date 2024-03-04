6cbb8600f25752d161f1997b1d7b7f51cfda2140
Leonardo Santos <leonardojosantos@gmail.com>
2024-03-04 17:27:57 -0300

    362. Integration between Shopping-Cart.ts with Discount.ts

--- src/classes/shoppingCartClass.spec.ts
+++ src/classes/shoppingCartClass.spec.ts
@@ -72,4 +72,18 @@ describe('ShoppingCart', () => {
     sut.removeItem(0);
     expect(sut.isEmpty()).toBe(true);
   });
+
+  it('should call discount.calculate once when totalWithDiscount is called', () => {
+    const { sut, discountMock } = createSutWithProducts();
+    const discountMockSpy = jest.spyOn(discountMock, 'calculate');
+    sut.totalItemsWithDiscount();
+    expect(discountMockSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call discount.calculate with total price when totalWithDiscount is called', () => {
+    const { sut, discountMock } = createSutWithProducts();
+    const discountMockSpy = jest.spyOn(discountMock, 'calculate');
+    sut.totalItemsWithDiscount();
+    expect(discountMockSpy).toHaveBeenCalledWith(sut.totalItems());
+  });
 });
